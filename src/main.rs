50dafe2012b19e087aca23ddb78ec112a1f68e3c
vilayat-ali <vilayatcodemysite@gmail.com>
2023-05-22 20:06:37 +0530

    Removed unnecessary docs HTML

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-use actix_files as fs;
-use actix_web::{web, App, HttpServer};
-
 // routes
 use auth_api::routes::docs::serve_docs;
 use auth_api::routes::{login, register};
@@ -8,14 +5,11 @@ use auth_api::routes::{login, register};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(
-                web::scope("/api")
-                    .route("/register", web::post().to(register))
-                    .route("/login", web::post().to(login)),
-            )
-            .service(fs::Files::new("/pages", ".").show_files_listing())
-            .route("/docs", web::get().to(serve_docs))
+        App::new().service(
+            web::scope("/api")
+                .route("/register", web::post().to(register))
+                .route("/login", web::post().to(login)),
+        )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
